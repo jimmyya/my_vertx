74a2d673685c6ff435115c9023786595ea84e70c
Jimmyya <812752853@qq.com>
2016-09-14 19:12:12 +0800

    不使用8080端口的配置

--- my_vert1/src/main/java/com/vert/io/MyFirstVerticle1.java
+++ my_vert1/src/main/java/com/vert/io/MyFirstVerticle1.java
@@ -7,33 +7,31 @@ import io.vertx.core.Future;
  * Created by CHEN on 2016/9/13.
  * 不使用8080端口
  */
-public class MyFirstVerticle1 extends AbstractVerticle{
+public class MyFirstVerticle1 extends AbstractVerticle {
 
     /**
-     * verticle部署之后，会自动调用start方法，
-     * start 方法接收Future对象 可以告诉用户执行完成还是报出错误
-     * Vert.x是异步执行的，运行的时候，不会等到start方法完成
-     * 所以，Future参数是非常重要的，可以通知方法是否已经执行完毕
+     * verticle部署之后，会自动调用start方法。
+     * start 方法接收Future对象 可以告诉用户执行完成还是报出错误。
+     * Vert.x是异步执行的，运行的时候，不会等到start方法完成。
+     * 所以，Future参数是非常重要的，可以通知方法是否已经执行完毕。
      *
      * @param fut
      * @throws Exception
      */
     @Override
-    public void start(Future<Void> fut) throws Exception {
-        vertx
-                .createHttpServer()
-                .requestHandler(r->{
-                    r.response().end("<h1>Hello from my first" +
-                            "Vert.x 3 application</h1>");
+    public void start(final Future<Void> fut) {
+        vertx.createHttpServer()
+                .requestHandler(r -> {
+                    r.response().end("<h1>Hello from my first"
+                            + "Vert.x 3 application</h1>");
                 })
                 .listen(
-                        config().getInteger("http.port",8080)
-                        ,result->{
-                   if(result.succeeded()) {
-                       fut.complete();
-                   } else {
-                       fut.fail(result.cause());//报告错误
-                   }
-                });
+                        config().getInteger("http.port", 8080), result -> {
+                            if (result.succeeded()) {
+                                fut.complete();
+                            } else {
+                                fut.fail(result.cause()); //报告错误
+                            }
+                        });
     }
 }
